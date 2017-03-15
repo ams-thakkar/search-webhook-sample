a2da0c1b9b86ed5b755a57240839ebcc5fc2212d
Thakkar, Amrish (at624x) <at624x@us.att.com>
2017-03-15 04:09:29 -0500

    complete unit tested code for calling /search

--- start.js
+++ start.js
@@ -2,8 +2,30 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var http = require('http');
 
 const apiRoute = express();
+
+
+//var url = 'http://services.att.com/search/v1/global-search?app-id=testing&start=0&row=1&fl=title&fq=_lw_data_source_s:globalsearch-catalog&facet=false&hl=false&q=';
+
+function getResponse(url, callback){
+    http.get(url, function(res) {
+    	var data = '';
+        res.on('data', function (chunk) {
+        	data += chunk;
+             });
+        res.on('end', function(){
+        	callback(JSON.parse(data));
+        	});
+        res.on('error', function (e) {
+            console.error(e);
+        	});
+        });
+	}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 apiRoute.use(bodyParser.urlencoded({
     extended: true
@@ -13,6 +35,15 @@ apiRoute.use(bodyParser.json());
 
 apiRoute.post('/find', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.apiAiText ? req.body.result.parameters.apiAiText : "Could not quite understand what you just said."
+	//var speech = 'galaxy';
+	var url = 'http://services.att.com/search/v1/global-search?app-id=testing&start=0&row=1&fl=title&fq=_lw_data_source_s:globalsearch-catalog&facet=false&hl=false&sort=defaultMarketingSequence%20asc&q='+speech;
+
+    console.log('you entered - ',url);
+    
+   	getResponse(url, function(results){
+    	console.log('results: ',results.response.docs[0].title[0]);
+    });
+    
     return res.json({
         speech: speech,
         displayText: speech,
@@ -22,6 +53,6 @@ apiRoute.post('/find', function(req, res) {
 
 
 
-apiRoute.listen((process.env.PORT || 8280), function() {
+apiRoute.listen((process.env.PORT || 8180), function() {
 	console.log("Server is up and listening...");
 });
